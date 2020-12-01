bb86d18065af0a58b8a18b3c320d12e43aeb1d97
Kevin Daniel <Dsanchez0397@gmail.com>
2020-12-01 08:26:07 -0600

    welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,12 +3,12 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>Laravel</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
+        <link rel="stylesheet" href="css/app.css">
         <!-- Styles -->
         <style>
             html, body {
@@ -80,6 +80,7 @@
             @endif
 
             <div class="content">
+                <div id="example"></div>
                 <div class="title m-b-md">
                     Laravel
                 </div>
@@ -97,4 +98,5 @@
             </div>
         </div>
     </body>
+    <script type="text/javascript" src="js/app.js"></script>
 </html>
